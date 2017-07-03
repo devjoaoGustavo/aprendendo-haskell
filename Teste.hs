7e835044e31e1c5dfe6d295ef05e79936be1a7f5
João Gustavo de Paula <dev.joaogustavo@gmail.com>
2017-07-02 23:30:36 -0300

    Enunciados dos próximos exercícios

--- Teste.hs
+++ Teste.hs
@@ -25,6 +25,7 @@ foo :: Char -> Int ->(Int, String)
 foo x y = (y+9, x:[x])
 
 -- Lista de exercícios do capítulo 1 do Livro de Haskell da casa do código
+--
 -- [1.0,11.0,121.0,1331.0,14641.0,161051.0,1771561.0]
 -- Deveria ser assim:
 -- [1,11,121,1331,14641,161051,1771561]
@@ -38,6 +39,28 @@ quatro = [x | x<-[1 .. 40], (rem x 4) /= 0]
 -- ["AaBB","AbBB","AcBB","AdBB","AeBB","AfBB","AgBB"]
 letras :: [[Char]]
 letras = [head "ABB" : x : tail "ABB" | x<-['a' ..'g']]
+
+-- [5,8,11,17,20,26,29,32,38,41]
+
+-- [1.0,0.5,0.25,0.125,0.0625,0.03125]
+
+-- [1,10,19,28,37,46,55,64]
+
+-- [2,4,8,10,12,16,18,22,24,28,30]
+
+-- [2,4,8,10,12,16,18,22,24,28,30]
+
+-- Crie uma função que verifique se o tamanho de uma String é par ou não. Use Bool como retorno.
+
+-- Escreva uma função que receba um vetor de Strings e retorne uma lista com todos os elementos em ordem reversa
+
+-- Escreva uma função que receba um vetor de Strings e retorne uma lista com o tamanho de cada String. As palavras de tamanho par devem ser exlcluídas da resposta.
+
+-- Escreva a função head como composição de duas outras.
+
+-- Faça uma função que receba uma String e retorne True se esta for um palíndromo; caso contrário False.
+
+-- Faça uma função que receba um inteiro e retorne uma tupla, contendo: o dobro deste número na primeira coordenada, o triplo na segunda, o quádrulo na terceira e o quíntuplo na quarta.
 
 
 
